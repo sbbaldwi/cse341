34b869ebce5813174b095a638c4dc092b16b2bd6
sbbaldwin <bal20020@byui.edu>
2024-01-10 16:15:52 -0700

    figured it out

--- week2/index.js
+++ week2/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
+
+app.use(express.static('public'));
 
 const professionalData = {
     professionalName: 'John Doe',
@@ -25,10 +29,10 @@ const professionalData = {
 };
 
 app.get('/professional', (req, res) => {
+    console.log('Request to /professional received');
     res.json(professionalData);
-});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
